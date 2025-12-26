0673c176f25379f6c0636fd3270afb7057224241
Y_Chanphyrat(Y_Chanphyrat@hunesion.com) <y_chanphyrat@hunesion.com>
2025-12-26 08:46:29 +0700

    init file

---
+++ src/main/java/com/hunesion/testdemor2dbc/TestDemoR2DbcApplication.java
@@ -0,0 +1,13 @@
+package com.hunesion.testdemor2dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDemoR2DbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDemoR2DbcApplication.class, args);
+    }
+
+}
